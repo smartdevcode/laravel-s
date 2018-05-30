5ea9b6054f002deca9e9e3fba1fee92646994485
SeniorDevCode <senior.dev.software@gmail.com>
2018-05-30 10:35:15 +0800

    more rewarders

--- src/Illuminate/Laravel.php
+++ src/Illuminate/Laravel.php
@@ -2,14 +2,11 @@
 
 namespace Hhxsv5\LaravelS\Illuminate;
 
-use Hhxsv5\LaravelS\Illuminate\Database\ConnectionFactory;
-use Hhxsv5\LaravelS\Illuminate\Database\DatabaseManager;
-use Illuminate\Support\Facades\Facade;
-use Illuminate\Http\Request as IlluminateRequest;
 use Illuminate\Contracts\Http\Kernel as HttpKernel;
 use Illuminate\Contracts\Console\Kernel as ConsoleKernel;
+use Illuminate\Http\Request as IlluminateRequest;
+use Illuminate\Support\Facades\Facade;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use Hhxsv5\LaravelS\Illuminate\Database\DatabaseServiceProvider;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 class Laravel
@@ -55,7 +52,6 @@ class Laravel
         $this->createKernel();
         $this->setLaravel();
         $this->consoleKernelBootstrap();
-        $this->registerServiceProviders();
         $this->saveSnapshots();
     }
 
@@ -232,14 +228,9 @@ class Laravel
         return $rsp;
     }
 
-    protected function registerServiceProviders()
+    public function reRegisterServiceProvider($providerCls, array $clearFacades = [])
     {
-        $this->reRegisterServiceProvider(DatabaseServiceProvider::class, ['db', 'db.factory', 'db.connection'], true);
-    }
-
-    public function reRegisterServiceProvider($providerCls, array $clearFacades = [], $force = false)
-    {
-        if (class_exists($providerCls, false) || $force) {
+        if (class_exists($providerCls, false)) {
             if ($this->conf['is_lumen']) {
                 $loadedProviders = $this->laravelReflect->getProperty('loadedProviders');
                 $loadedProviders->setAccessible(true);
